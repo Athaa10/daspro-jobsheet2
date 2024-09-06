b90a0702f79c15940a8fa31e4b6d3c22616d9679
Athaa10 <athamaulidia0904@gmail.com>
2024-09-06 20:35:29 +0700

    simpan kode java ke Github

---
+++ DataDiri03.java
@@ -0,0 +1,15 @@
+public class DataDiri03 {
+    public static void main(String[] args){
+        System.out.println(x:"-------------------------------------------------------");
+        System.out.println(x:"--Data Mahasiswa jurusan Teknologi Informasi POLINEMA--");
+        System.out.println(x:"-------------------------------------------------------");
+        System.out.println(x:"Nama       :ATHA MAULIDIA");
+        System.out.println(X:"NIM        :244107060080");
+        System.out.println(X:"Kelas      :1C");
+        System.out.println(X:"No Absen   :03");
+        System.out.println(X:"Prodi      :D-IV Sistem Informasi Bisnis");
+        System.out.println(X:"Alamat     :Pati");
+        System.out.println(X:"No HP      :085952644284");
+        System.out.println(X:"Hobi       :Bermain Game");
+    }
+}
